4a0cd23f2e1f07b45603368e233d239e6dff59c3
lucas <974860016@qq.com>
2020-02-25 19:08:56 +0800

    20200225

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@
  * @Author: LucasYan
  * @Date: 2020-02-24 17:44:38
  * @LastEditors: LucasYan
- * @LastEditTime: 2020-02-24 18:29:58
+ * @LastEditTime: 2020-02-25 19:05:12
  * @Description:
  */
 
 const global_w = window.innerWidth,
   global_h = window.innerHeight;
+
+const min = global_w < global_h ? global_w : global_h;
+const max = global_w > global_h ? global_w : global_h;
+const scale = min / 750;
 
 class LongImage {
   constructor(options) {
@@ -19,9 +23,13 @@ class LongImage {
     this.height = options.height || global_h;
     this.container = options.container || document.body;
     this.resources = options.resources || [];
+    this.scale = Math.min(this.width, this.height) / 750;
+    this.preVal = 0; //存储滑动的距离
+    this.phyTouch = null;
+    this.isLocked = false; // 锁住位置变化
     this.autoLoad =
       typeof options.autoLoad === "undefined" ? true : options.autoLoad; // 是否自动加载
-
+    this.touchOptions = options.touchOptions || {};
     this.app = new PIXI.Application({
       resolution: 1, //resolution让Pixi在不同的分辨率和像素密度的平台上运行变得简单
       transparent: true,
@@ -41,35 +49,179 @@ class LongImage {
     that.app.loader
       .add(this.resources)
       .on("progress", this.loadProgress.bind(this))
-      .load(this.loadDone.bind(this))
+      .load(this.loadDone.bind(this));
   }
 
   loadProgress(loader) {
     this.trigger("loadProgress", loader.progress);
   }
 
-//   加载资源结束之后开始执行方法
+  //   加载资源结束之后开始执行方法
   loadDone() {
-      const that = this
+    const that = this;
     //   创建场景
-    this.initFirstScene()
-  }
+    this.initFirstScene();
 
-
-//   初始化第一个场景
-  initFirstScene() {
-    const that = this
-    const sceneResource = ['new-1-1', 'new-1-2', 'new-1-3']
-    that.firstScene = new PIXI.Container()
-    sceneResource.forEach(item => {
-        const bg = new PIXI.Sprite(that.app.loader.resources[item].texture)
-        bg.x = 0
-        that.firstScene.addChild(bg)
-    })
-    that.app.stage.addChild(this.firstScene);
+    // 设置可滚动的长度
+    this.originWidth = this.app.stage.width;
+    this.scrollWidth = this.app.stage.width * scale - max;
+    // this.scrollWidth = 3000;
     that.app.stage.interactive = true;
+
+    this.app.stage.scale.set(this.scale, this.scale);
+
+    if (this.width < this.height) {
+      // 根据横屏竖屏效果旋转舞台
+      this.app.stage.rotation = Math.PI / 2;
+      this.app.stage.x = this.width;
+      this.initTouch(true, "y", 0);
+    } else {
+      this.initTouch(false, "x", 0);
+    }
+
+    // 画布渲染
+    this.setup();
   }
 
+  setup() {
+    const that = this;
+    that.app.ticker.add(delta => {
+      that.app.renderer.render(that.app.stage);
+    });
+  }
+
+  //   初始化第一个场景
+  initFirstScene() {
+    const that = this;
+    
+    // 第一个场景container
+    that.firstScene = new PIXI.Container();
+    
+    /****** 加载第一个场景左边的部分 ******/
+
+    const sceneResource_left = ["new-1-1", "new-1-2", "new-1-3"];
+    const resourceSize_left = [
+      { x: 0, y: 0 }, //天空
+      { x: 290, y: 300 }, //房子
+      { x: -200, y: 0 } //树
+    ];
+
+    that.leftScene = new PIXI.Container();
+
+    // 加载场景背景
+    sceneResource_left.forEach((item, index) => {
+      if (index === 0) {
+        // 加载天空动画
+        const skys = that.spritesAnimations("new-1-sky", 11);
+        skys.animationSpeed = -0.15;
+        // skys.scale.set(0.5, 0.5);
+        skys.position.set(0, 0);
+        skys.play();
+        that.leftScene.addChild(skys);
+      } else {
+        // 加载树和屋子
+        const bg = new PIXI.Sprite(that.app.loader.resources[item].texture);
+        bg.x = resourceSize_left[index].x;
+        bg.y = resourceSize_left[index].y;
+        // bg.scale.set(0.5, 0.5);
+        that.leftScene.addChild(bg);
+      }
+    });
+
+    // 加载乌鸦
+    const crows = that.spritesAnimations("new-1-bird", 21);
+    crows.animationSpeed = -0.15;
+    // crows.scale.set(0.5);
+    crows.position.set(130, 200);
+    crows.play();
+    that.leftScene.addChild(crows);
+
+    /****** 加载第一个场景右边 ******/
+    that.rightScene = new PIXI.Container();
+    that.rightScene.x = that.leftScene.width - 300
+    that.rightScene.width = 1400
+    const sceneResource_right = ['new-2-1', 'new-2-2', 'new-2-3']
+    const resourceSize_rigth = [
+      {x: 0, y: 0},
+      {x: 500, y: 500},
+      {x: 0, y: 0},
+    ]
+    sceneResource_right.forEach((item, index) => {
+      const bg = new PIXI.Sprite(that.app.loader.resources[item].texture);
+        bg.x = resourceSize_rigth[index].x;
+        bg.y = resourceSize_rigth[index].y;
+        that.rightScene.addChild(bg);
+    })
+    
+
+
+
+    /****** 加载第一个场景中间的树 ******/
+
+    that.middleScene = new PIXI.Container();
+    const bg = new PIXI.Sprite(that.app.loader.resources['tree'].texture);
+
+    
+    // 加载树
+
+    const tree = new PIXI.Sprite(that.app.loader.resources['tree'].texture);
+    tree.position.set(that.leftScene.width - 450, 0)
+    that.middleScene.addChild(tree)
+    
+    that.firstScene.addChild(this.leftScene)
+    that.firstScene.addChild(this.rightScene)
+    that.firstScene.addChild(this.middleScene)
+
+    that.app.stage.addChild(this.firstScene);
+  }
+
+  // 加载精灵图
+  spritesAnimations(prefix, count) {
+    const that = this,
+      textureArray = [];
+    for (let i = 1; i <= count; i++) {
+      textureArray.unshift(that.app.loader.resources[`${prefix}-${i}`].texture);
+    }
+    const animatedSprite = new PIXI.AnimatedSprite(textureArray);
+    return animatedSprite;
+  }
+
+  initTouch(verticalBoolen, ver, value = 0) {
+    let vertical = verticalBoolen, //默认是true代表监听竖直方向touch
+      storeVal = ver,
+      that = this;
+    this.phyTouch = new PhyTouch(
+      Object.assign({ touch: "body" }, that.touchOptions, {
+        // touch: "#app", //反馈触摸的dom
+        vertical: verticalBoolen, //默认是true代表监听竖直方向touch
+        min: -that.scrollWidth, //不必需,运动属性的最小值
+        max: 0, //不必需,滚动属性的最大值
+        maxSpeed: 1, //摸反馈的最大速度限制
+        value,
+        bindSelf: true,
+        change(value) {
+          if (!value && value !== 0) {
+            _this.phyTouch.destroy();
+            _this.phyTouch = _this.initTouch(ver, storeVal, _this.preVal);
+          } else {
+            if (!that.isLocked) {
+              // 锁定方向才改变stage位置
+              //连接pixi
+              let stageScrollX; //stage位置变化
+              if (value < -that.scrollWidth / 2) {
+                stageScrollX =
+                  value < -that.scrollWidth ? -that.scrollWidth : value;
+              } else {
+                stageScrollX = value > 0 ? 0 : value;
+              }
+              that.app.stage.position[ver] = stageScrollX;
+              console.log("after value: ", stageScrollX);
+            }
+          }
+        }
+      })
+    );
+  }
 
   // 事件处理
   on(name, callback) {
@@ -98,9 +250,14 @@ class LongImage {
 
 const longImage = new LongImage({
   container: document.getElementById("app"),
-  resources: resources
+  resources: resources,
+  touchOptions: {
+    initialValue: 0,
+    sensitivity: 0.8,
+    maxSpeed: 0.6
+  }
 });
 
-longImage.on('loadProgress', progress => {
-    console.log(progress)
-})
+longImage.on("loadProgress", progress => {
+  console.log(progress);
+});
--- js/data.js
+++ js/data.js
@@ -2,12 +2,48 @@
  * @Author: LucasYan
  * @Date: 2020-02-24 17:56:28
  * @LastEditors: LucasYan
- * @LastEditTime: 2020-02-24 18:08:34
+ * @LastEditTime: 2020-02-25 18:57:39
  * @Description: 
  */
 
  const resources = [
      {url: 'imgs/new-1-1.jpg', name: 'new-1-1'},
      {url: 'imgs/new-1-2.png', name: 'new-1-2'},
-     {url: 'imgs/new-1-3.png', name: 'new-1-3'}
+     {url: 'imgs/new-1-3.png', name: 'new-1-3'},
+     {url: 'imgs/new-1-bird-1.png', name: 'new-1-bird-1'},
+     {url: 'imgs/new-1-bird-2.png', name: 'new-1-bird-2'},
+     {url: 'imgs/new-1-bird-3.png', name: 'new-1-bird-3'},
+     {url: 'imgs/new-1-bird-4.png', name: 'new-1-bird-4'},
+     {url: 'imgs/new-1-bird-5.png', name: 'new-1-bird-5'},
+     {url: 'imgs/new-1-bird-6.png', name: 'new-1-bird-6'},
+     {url: 'imgs/new-1-bird-7.png', name: 'new-1-bird-7'},
+     {url: 'imgs/new-1-bird-8.png', name: 'new-1-bird-8'},
+     {url: 'imgs/new-1-bird-9.png', name: 'new-1-bird-9'},
+     {url: 'imgs/new-1-bird-10.png', name: 'new-1-bird-10'},
+     {url: 'imgs/new-1-bird-11.png', name: 'new-1-bird-11'},
+     {url: 'imgs/new-1-bird-12.png', name: 'new-1-bird-12'},
+     {url: 'imgs/new-1-bird-13.png', name: 'new-1-bird-13'},
+     {url: 'imgs/new-1-bird-14.png', name: 'new-1-bird-14'},
+     {url: 'imgs/new-1-bird-15.png', name: 'new-1-bird-15'},
+     {url: 'imgs/new-1-bird-16.png', name: 'new-1-bird-16'},
+     {url: 'imgs/new-1-bird-17.png', name: 'new-1-bird-17'},
+     {url: 'imgs/new-1-bird-18.png', name: 'new-1-bird-18'},
+     {url: 'imgs/new-1-bird-19.png', name: 'new-1-bird-19'},
+     {url: 'imgs/new-1-bird-20.png', name: 'new-1-bird-20'},
+     {url: 'imgs/new-1-bird-21.png', name: 'new-1-bird-21'},
+     {url: 'imgs/new-1-sky-1.jpg', name: 'new-1-sky-1'},
+     {url: 'imgs/new-1-sky-2.jpg', name: 'new-1-sky-2'},
+     {url: 'imgs/new-1-sky-3.jpg', name: 'new-1-sky-3'},
+     {url: 'imgs/new-1-sky-4.jpg', name: 'new-1-sky-4'},
+     {url: 'imgs/new-1-sky-5.jpg', name: 'new-1-sky-5'},
+     {url: 'imgs/new-1-sky-6.jpg', name: 'new-1-sky-6'},
+     {url: 'imgs/new-1-sky-7.jpg', name: 'new-1-sky-7'},
+     {url: 'imgs/new-1-sky-8.jpg', name: 'new-1-sky-8'},
+     {url: 'imgs/new-1-sky-9.jpg', name: 'new-1-sky-9'},
+     {url: 'imgs/new-1-sky-10.jpg', name: 'new-1-sky-10'},
+     {url: 'imgs/new-1-sky-11.jpg', name: 'new-1-sky-11'},
+     {url: 'imgs/tree.png', name: 'tree'},
+     {url: 'imgs/new-2-1.jpg', name: 'new-2-1'},
+     {url: 'imgs/new-2-2.png', name: 'new-2-2'},
+     {url: 'imgs/new-2-3.png', name: 'new-2-3'},
  ]
